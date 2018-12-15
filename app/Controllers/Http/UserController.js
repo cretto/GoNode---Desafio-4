789e0f01f148fd4807ecd20f9403355733d5cdbf
cretto <creto_boy@hotmail.com>
2018-12-15 12:32:38 -0200

    GoNode - Desafio 4 - Criado SessionController e adicionado update em  UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Hash = use('Hash')
 
 class UserController {
   async store ({ request }) {
@@ -9,6 +10,37 @@ class UserController {
     const user = await User.create(data)
 
     return user
+  }
+
+  async update ({ request, response, auth }) {
+    try {
+      const { name, password, old_password: oldPassword } = request.all()
+
+      const user = await User.findByOrFail('id', auth.user.id)
+
+      if (oldPassword && !password) {
+        return response
+          .status(400)
+          .send({ error: { message: 'The new password does not be empty' } })
+      }
+
+      if (!(await Hash.verify(oldPassword, user.password))) {
+        return response
+          .status(400)
+          .send({ error: { message: 'The old password does not match' } })
+      }
+
+      if (name) user.name = name
+      if (password) user.password = password
+
+      await user.save()
+
+      return user
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Something went wrong!' } })
+    }
   }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -2,4 +2,9 @@
 
 const Route = use('Route')
 
-Route.post('users', 'UserController.store').validator('User')
+Route.post('/users', 'UserController.store').validator('User')
+Route.post('/sessions', 'SessionController.store').validator('Session')
+
+Route.group(() => {
+  Route.put('/users', 'UserController.update').validator('UpdateUser')
+}).middleware(['auth'])
